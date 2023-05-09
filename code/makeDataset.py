19783ff5b5cf09f4644f91442a63b5b5b7916479
adriend1102 <114516137+adriend1102@users.noreply.github.com>
2023-05-09 11:28:06 +0200

    resize dataset

--- code/makeDataset.py
+++ code/makeDataset.py
@@ -6,16 +6,17 @@ Creation dataset from videos
 import cv2
 from scipy import misc
 from PIL import Image
-            
+import numpy as np    
 
 def getFrames(file, destination, c):
-    
+    TAMANO_IMG=100
     print("Reading " + file)
     vidcap = cv2.VideoCapture(file)
     success,image = vidcap.read()
         
     print("Saving frames in " + destination)
     while success:
+        image = cv2.resize(image, (150,100))
         cv2.imwrite(destination +  "/sign_" + sign + "_" + str(c) + ".jpg", image)      
         success,image = vidcap.read()
         c += 1
@@ -35,17 +36,17 @@ if __name__ == '__main__':
         
         # Llegir opcions a + c
         for opcAC in listOpcAC:
-            fileA = "../dataset/videos/" + sign + opcAC + "a" + ".mp4"
-            fileC = "../dataset/videos/" + sign + opcAC + "c" + ".MOV"
-            destination = "../dataset/sign_" + sign
+            fileA = "dataset/videos/" + sign + opcAC + "a" + ".mp4"
+            fileC = "dataset/videos/" + sign + opcAC + "c" + ".MOV"
+            destination = "dataset/sign_" + sign
             c = getFrames(fileA, destination, c)
             c = getFrames(fileC, destination, c)
             
         
         # Llegir opcions b
         for opc in listOpcB:
-            file = "../dataset/videos/" + sign + opc + ".mp4"
-            destination = "../dataset/sign_" + sign
+            file = "dataset/videos/" + sign + opc + ".mp4"
+            destination = "dataset/sign_" + sign
             c = getFrames(file, destination, c)
             
         countSigns[i] += c
